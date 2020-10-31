fe2200ab6d41b7ef1bcc24cd76c7a1b5fc2ecc1d
Languiller <Daniel.Languiller@team.telstra.com>
2020-10-31 21:56:44 +1100

    WIP

--- Edabit/arrays.js
+++ Edabit/arrays.js
@@ -63,4 +63,67 @@ function nextElement(arr) {
 // 	return arr[arr.length - 1] + (arr[1] - arr[0]);
 // }
 
-console.log(nextElement([3, 5, 7, 9]));
+function canCapture([yourRook, opponentsRook]) {
+  return yourRook[0] === opponentsRook[0] || yourRook[1] === opponentsRook[1];
+}
+
+function findBob(names) {
+  return names.indexOf("Bob");
+}
+
+function oddProduct(arr) {
+  return arr.filter((el) => el % 2 !== 0).reduce((acc, el) => (el *= acc), 1);
+}
+
+function countdown(start) {
+  let arr = [];
+  for (let i = start; i >= 0; i--) {
+    arr.push(i);
+  }
+  return arr;
+}
+// Slick Daddy
+// const countdown = start => [...Array(start + 1).keys()].reverse();
+
+function formatDate(date) {
+  return date.split("/").reverse().join("");
+}
+
+function transform(arr) {
+  return arr.map((el) => (el % 2 === 0 ? el - 1 : el + 1));
+}
+
+function unlucky13(nums) {
+  return nums.filter((el) => el % 13 !== 0);
+}
+
+function mysteryFunc(arr, num) {
+  return arr.map((el) => el % num);
+}
+
+function filterDigitLength(arr, num) {
+  return arr.filter((el) => el.toString().length === num);
+}
+
+function checkEquals(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+// Slick Daddy
+// function checkEquals(arr1, arr2) {
+// 	return arr1.every((x, i) => x === arr2[i])
+// }
+
+function correctStream(user, correct) {
+  return user.map((el, i) => (el === correct[i] ? 1 : -1));
+}
+
+function greetPeople(names) {
+  return names.map((el) => `Hello ${el}`).join(", ");
+}
+
+console.log(greetPeople(["Kyrill", "Mom", "Dad", "Zuzu"]));
+console.log(greetPeople([]));
