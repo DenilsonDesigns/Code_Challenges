b6358027b9ada273c50ea572e42955cf3cf21273
Languiller <Daniel.Languiller@team.telstra.com>
2021-04-17 15:22:45 +1000

    leetcode 1662

--- LeetCode/mathEasy.js
+++ LeetCode/mathEasy.js
@@ -24,4 +24,25 @@ function arraySign(nums) {
   return prod === 0 ? 0 : prod > 0 ? 1 : -1;
 }
 
-console.log();
+// 1281. Subtract the Product and Sum of Digits of an Integer
+// Given an integer number n, return the difference between
+// the product of its digits and the sum of its digits.
+
+function subtractProductAndSum(n) {
+  let s = 0;
+  let p = 1;
+
+  let digits = n
+    .toString()
+    .split("")
+    .map((el) => parseInt(el));
+
+  for (let i = 0; i < digits.length; i++) {
+    p *= digits[i];
+    s += digits[i];
+  }
+
+  return p - s;
+}
+
+console.log(subtractProductAndSum(234)); // 15
--- LeetCode/stringEasy.js
+++ LeetCode/stringEasy.js
@@ -41,4 +41,59 @@ function halvesAreAlike(s) {
   return firstHalfVowels === secondHalfVowels;
 }
 
-console.log(halvesAreAlike("textbook"));
+// 1221. Split a String in Balanced Strings
+// Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
+
+// Given a balanced string s, split it in the maximum amount of balanced strings.
+
+// Return the maximum amount of split balanced strings.
+/**
+ * @param {string} s Chain of R's and L's
+ * @returns {number} count of balanced strings.
+ */
+function balancedStringSplit(s) {
+  // Time: O(n) | Space: O(4) => O(1)
+  let r = 0;
+
+  let leftIdx = 0;
+  let rCount = 0;
+  let lCount = 0;
+  while (leftIdx < s.length) {
+    if (s[leftIdx] === "R") {
+      rCount++;
+    } else if (s[leftIdx] === "L") {
+      lCount++;
+    }
+
+    if (rCount > 0 && rCount === lCount) {
+      r++;
+      lCount = 0;
+      rCount = 0;
+    }
+
+    leftIdx++;
+  }
+
+  return r;
+}
+
+// 1662. Check If Two String Arrays are Equivalent
+// Given two string arrays word1 and word2,
+// return true if the two arrays represent the same string, and false otherwise.
+
+// A string is represented by an array if the array elements
+// concatenated in order forms the string.
+/**
+ *
+ * @param {array[strings]} word1
+ * @param {array[strings]} word2
+ * @returns {boolean} true/false if joined stringified word1 and
+ * word2 are equivalent.
+ */
+function arrayStringsAreEqual(word1, word2) {
+  // let words1Str = word1.join("");
+
+  return word1.join("") === word2.join("");
+}
+
+console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"]));
