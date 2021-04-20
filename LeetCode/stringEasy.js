77945be959fcec6cc2302ff3e5c1957b87c3281e
Languiller <Daniel.Languiller@team.telstra.com>
2021-04-20 19:45:43 +1000

    leetcode 1768

--- LeetCode/stringEasy.js
+++ LeetCode/stringEasy.js
@@ -287,4 +287,29 @@ function checkIfPangram(sentence) {
   return alpha === compStr;
 }
 
-console.log(checkIfPangram("leetcode"));
+// 1768. Merge Strings Alternately
+// You are given two strings word1 and word2.
+// Merge the strings by adding letters in alternating order,
+// starting with word1. If a string is longer than the other,
+// append the additional letters onto the end of the merged string.
+
+// Return the merged string.
+/**
+ *
+ * @param {string} word1
+ * @param {string} word2
+ * @returns {string} merged string of word1 and word2
+ */
+function mergeAlternately(word1, word2) {
+  let wLength = Math.max(word1.length, word2.length);
+  let r = "";
+
+  for (let i = 0; i < wLength; i++) {
+    if (word1[i]) r += word1[i];
+    if (word2[i]) r += word2[i];
+  }
+  return r;
+}
+
+console.log(mergeAlternately("ab", "pqrs"));
+// a p b q cd
