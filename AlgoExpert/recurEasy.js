ebe556fda6b7ff316773288e94ebfc3de8cae7f7
Languiller <Daniel.Languiller@team.telstra.com>
2021-04-20 13:57:38 +1000

    algo expert recursion problems

---
+++ AlgoExpert/recurEasy.js
@@ -0,0 +1,22 @@
+function getNthFib(n, memoize = { 1: 0, 2: 1 }) {
+  if (n in memoize) {
+    return memoize[n];
+  } else {
+    memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n - 2, memoize);
+    return memoize[n];
+  }
+}
+
+function productSum(array, multiplier = 1) {
+  let sum = 0;
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      sum += productSum(element, multiplier + 1);
+    } else {
+      sum += element;
+    }
+  }
+  return sum * multiplier;
+}
+
+console.log(productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]));
