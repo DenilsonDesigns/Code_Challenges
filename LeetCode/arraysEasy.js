3c844ec1123a148fa6c719be31cdf8044666e6db
Languiller <Daniel.Languiller@team.telstra.com>
2021-04-21 15:40:01 +1000

    leetcode 1827

--- LeetCode/arraysEasy.js
+++ LeetCode/arraysEasy.js
@@ -963,4 +963,35 @@ function sumOddLengthSubarrays(arr) {
   }
 }
 
-console.log(sumOddLengthSubarrays([1, 4, 2, 5, 3]));
+// 1827. Minimum Operations to Make the Array Increasing
+// You are given an integer array nums (0-indexed).
+// In one operation, you can choose an element of the array
+// and increment it by 1.
+
+// For example, if nums = [1,2,3], you can choose to increment
+// nums[1] to make nums = [1,3,3].
+// Return the minimum number of operations needed to make nums strictly increasing.
+
+// An array nums is strictly increasing if nums[i] < nums[i+1]
+// for all 0 <= i < nums.length - 1. An array of length 1 is trivially
+// strictly increasing.
+/**
+ * @param {number[]} nums
+ * @returns {number} steps required to make strictly increasing.
+ */
+function minOperations(nums) {
+  let operations = 0;
+  // loop through, if nums[i] is not greater than nums[-1]
+  // changes nums[i] in place to be nums[i-1] +1 and
+  // add to operations: (nums[i-1] +1) - nums[i]
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] <= nums[i - 1]) {
+      operations += nums[i - 1] + 1 - nums[i];
+      nums[i] = nums[i - 1] + 1;
+    }
+  }
+
+  return operations;
+}
+
+console.log(minOperations([1, 5, 2, 4, 1]));
