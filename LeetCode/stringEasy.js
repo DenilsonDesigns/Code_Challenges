b32b7eb66102528ce0e641d43bc77c0c1d1fa8ad
Languiller <Daniel.Languiller@team.telstra.com>
2021-04-28 22:25:54 +1000

    leetcode 944

--- LeetCode/stringEasy.js
+++ LeetCode/stringEasy.js
@@ -397,4 +397,36 @@ function freqAlphabets(s) {
   return r;
 }
 
-console.log(freqAlphabets("10#11#12"));
+// 944. Delete Columns to Make Sorted
+// You are given an array of n strings strs, all of the same length.
+
+// The strings can be arranged such that there is one on each line,
+// making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as:
+
+// abc
+// bce
+// cae
+// You want to delete the columns that are not sorted lexicographically.
+// In the above example (0-indexed), columns 0 ('a', 'b', 'c')
+// and 2 ('c', 'e', 'e') are sorted while column 1 ('b', 'c', 'a') is not,
+// so you would delete column 1.
+
+// Return the number of columns that you will delete.
+/**
+ * @param {*} strs
+ * @returns {number} amount of columns that need to be deleted.
+ */
+function minDeletionSize(strs) {
+  let indexes = new Set();
+
+  for (let i = 1; i < strs.length; i++) {
+    for (let j = 0; j < strs[i].length; j++) {
+      if (strs[i][j] < strs[i - 1][j]) {
+        indexes.add(j);
+      }
+    }
+  }
+  return indexes.size;
+}
+
+console.log(minDeletionSize(["cba", "daf", "ghi"]));
