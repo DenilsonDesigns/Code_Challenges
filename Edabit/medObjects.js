35a3597fcfc70ab223f0091dcd03228bea510d0e
Daniel <daniellanguilller@gmail.com>
2021-05-24 22:28:45 +1000

    edabit session

--- Edabit/medObjects.js
+++ Edabit/medObjects.js
@@ -141,4 +141,218 @@ function expensiveOrders(orders, cost) {
   return Object.fromEntries(filtered);
 }
 
-console.log(expensiveOrders({ a: 3000, b: 200, c: 1050 }, 1000));
+// Burglary Series (22): Sign All
+/**
+ *
+ * @param {Object} obj
+ * @param {String} name
+ */
+function signAll(obj, name) {
+  for (let key of Object.keys(obj)) {
+    if (key === "signature") obj[key] = name;
+    else {
+      for (let key2 of Object.keys(obj[key])) {
+        if (key2 === "signature") {
+          obj[key][key2] = name;
+        }
+      }
+    }
+  }
+  return obj;
+}
+
+const obj234 = {
+  bathroom: {
+    stereo: 220,
+    signature: "",
+  },
+  cellar: {
+    mirror: 10,
+    broom: 90,
+    signature: "",
+  },
+  signature: "",
+};
+
+// Burglary Series (08): Remove an Entry
+function removeEntry(obj, itemName) {
+  let newObj = { ...obj };
+  delete newObj[itemName];
+  return newObj;
+}
+
+// Burglary Series (03): Is It Gone?
+/**
+ *
+ * @param {Object} obj
+ * @param {String} name
+ * @returns {String}
+ */
+function findIt(obj, name) {
+  const finalName = name.charAt(0).toUpperCase() + name.slice(1);
+  if (name in obj) {
+    return `${finalName} is gone...`;
+  }
+
+  return `${finalName} is here!`;
+}
+
+// Lowercase and Uppercase Map
+/**
+ *
+ * @param {String[]} letters
+ * @returns {Object}
+ */
+function mapping(letters) {
+  let rMap = {};
+
+  letters.forEach((char) => {
+    rMap[char] = char.toUpperCase();
+  });
+
+  return rMap;
+}
+
+// Invert Keys and Values
+/**
+ *
+ * @param {Object} o
+ * @returns {Object}
+ */
+function invert(o) {
+  let rMap = {};
+  for (let [k, v] of Object.entries(o)) {
+    rMap[v] = k;
+  }
+
+  return rMap;
+}
+
+// Count the Letters and Digits
+/**
+ *
+ * @param {String} str
+ * @returns {Object}
+ */
+function countAll(str) {
+  let rMap = {
+    LETTERS: 0,
+    DIGITS: 0,
+  };
+
+  str.split("").forEach((char) => {
+    if (char === " ") return;
+    else Object.is(parseInt(char), NaN) ? rMap["LETTERS"]++ : rMap["DIGITS"]++;
+  });
+
+  return rMap;
+}
+
+// Learn Lodash (14) _.countBy, Count Items in an Array
+/**
+ *
+ * @param {Array[Object]} collection
+ * @param {String} iteratee
+ * @returns {Object}
+ */
+function countByValue(collection, iteratee) {
+  let rMap = {};
+
+  collection.forEach((element) => {
+    rMap[element[iteratee]]
+      ? rMap[element[iteratee]]++
+      : (rMap[element[iteratee]] = 1);
+  });
+
+  return rMap;
+}
+
+// Burglary Series (06): Convert to Number
+/**
+ *
+ * @param {Object} obj
+ * @returns {Object}
+ */
+function convertToNumber(obj) {
+  for (let [k, v] of Object.entries(obj)) {
+    obj[k] = parseInt(v);
+  }
+  return obj;
+}
+
+// Burglary Series (15): Number of Occurrences
+/**
+ *
+ * @param {Object} obj
+ * @returns {Object}
+ */
+function countNumberOfOccurrences(obj) {
+  let rMap = {};
+
+  for (let v of Object.values(obj)) {
+    rMap[v] ? rMap[v]++ : (rMap[v] = 1);
+  }
+
+  return rMap;
+}
+
+// Burglary Series (09): Filter Values
+/**
+ *
+ * @param {Object} obj
+ * @returns {Object}
+ */
+function filterValues(obj) {
+  let rMap = {};
+  for (let [k, v] of Object.entries(obj)) {
+    if (v >= 5000) rMap[k] = v;
+  }
+
+  return rMap;
+}
+
+// International Greetings
+const GUEST_LIST = {
+  Randy: "Germany",
+  Karla: "France",
+  Wendy: "Japan",
+  Norman: "England",
+  Sam: "Argentina",
+};
+function greeting(name) {
+  if (!GUEST_LIST[name]) return "Hi! I'm a guest.";
+  return `Hi! I'm ${name}, and I'm from ${GUEST_LIST[name]}.`;
+}
+
+// Who's The Oldest?
+/**
+ *
+ * @param {Object} people
+ * @returns {String}
+ */
+function oldest(people) {
+  let asArr = Object.entries(people).sort((a, b) => b[1] - a[1]);
+  return asArr[0][0];
+}
+
+// Leaderboard Sort
+/**
+ *
+ * @param {Array[Object]} users
+ * @returns {Array[Object]}
+ */
+function leaderboards(users) {
+  return users.sort((a, b) => getTrustScore(b) - getTrustScore(a));
+}
+
+function getTrustScore(userObj) {
+  return userObj["score"] + userObj["reputation"] * 2;
+}
+
+console.log(
+  leaderboards([
+    { name: "a", score: 100, reputation: 20 },
+    { name: "b", score: 90, reputation: 40 },
+    { name: "c", score: 115, reputation: 30 },
+  ])
+);
