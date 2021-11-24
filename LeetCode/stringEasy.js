effffb9e1d0f32abd7fe5069e49976d375ecbd04
Daniel Languiller <dlanguiller@opticomm.net.au>
2021-11-24 17:10:04 +1100

    24/11/2021

--- LeetCode/stringEasy.js
+++ LeetCode/stringEasy.js
@@ -655,8 +655,39 @@ function minMovesToSeat(seats, students) {
   return Math.abs(r);
 }
 
+// 1880. Check if Word Equals Summation of Two Words
+// The letter value of a letter is its position in the alphabet starting from 0
+// (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, etc.).
+
+// The numerical value of some string of lowercase English letters
+// s is the concatenation of the letter values of each letter in s,
+// which is then converted into an integer.
+
+// For example, if s = "acb", we concatenate each letter's letter value, resulting in "021".
+// After converting it, we get 21.
+
+// You are given three strings firstWord, secondWord, and targetWord,
+// each consisting of lowercase English letters 'a' through 'j' inclusive.
+
+// Return true if the summation of the numerical values of firstWord and
+// secondWord equals the numerical value of targetWord, or false otherwise.
+function isSumEqual(firstWord, secondWord, targetWord) {
+  const getWordNum = (word) => {
+    let r = "";
+
+    word.split("").forEach((el) => {
+      r += el.charCodeAt(0) - 97;
+    });
+    return parseInt(r);
+  };
+
+  return (
+    getWordNum(firstWord) + getWordNum(secondWord) === getWordNum(targetWord)
+  );
+}
+
 console.log(
   // **********************
-  minMovesToSeat([3, 1, 5], [2, 7, 4])
+  isSumEqual("acb", "cba", "cdb")
   // **********************
 );
