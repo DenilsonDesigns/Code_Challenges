693ca540a66800216b33b5a3a09fc119a5243f3d
DenilsonDesigns <daniellanguilller@gmail.com>
2022-02-22 13:19:55 +1100

    leetcode one

--- LeetCode/arraysEasy.js
+++ LeetCode/arraysEasy.js
@@ -1814,8 +1814,27 @@ function maxDistance(colors) {
   return maxDist;
 }
 
+// 575. Distribute Candies
+// Alice has n candies, where the ith candy is of type candyType[i].
+// Alice noticed that she started to gain weight, so she visited a doctor.
+
+// The doctor advised Alice to only eat n / 2 of the candies she
+// has (n is always even). Alice likes her candies very much,
+// and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+
+// Given the integer array candyType of length n, return
+// the maximum number of different types of candies she can
+// eat if she only eats n / 2 of them.
+function distributeCandies(candyType) {
+  const maxEdible = candyType.length / 2;
+
+  const uniqueCandiesCount = new Set(candyType).size;
+
+  return uniqueCandiesCount > maxEdible ? maxEdible : uniqueCandiesCount;
+}
+
 console.log(
   // ***********************
-  maxDistance([4, 4, 4, 11, 4, 4, 11, 4, 4, 4, 4, 4])
+  distributeCandies([1, 1, 2, 3])
   // ***********************
 );
