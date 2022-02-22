9b5341532b6ab000a33b7d5afd87578fb260d5b2
DenilsonDesigns <daniellanguilller@gmail.com>
2022-02-22 18:50:40 +1100

    leetcode few done

--- LeetCode/stringEasy.js
+++ LeetCode/stringEasy.js
@@ -895,8 +895,27 @@ function maxNumberOfBalloons(text) {
   return Math.floor(Math.min(...Object.values(wordMap)));
 }
 
+// 520. Detect Capital
+// We define the usage of capitals in a word to be right when
+// one of the following cases holds:
+
+// All letters in this word are capitals, like "USA".
+// All letters in this word are not capitals, like "leetcode".
+// Only the first letter in this word is capital, like "Google".
+// Given a string word, return true if the usage of capitals in it is right.
+function detectCapitalUse(word) {
+  if (
+    word === word.toUpperCase() ||
+    word === word.toLowerCase() ||
+    word === word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  )
+    return true;
+
+  return false;
+}
+
 console.log(
   // **********************
-  maxNumberOfBalloons("balon")
+  detectCapitalUse("ABaAB")
   // **********************
 );
