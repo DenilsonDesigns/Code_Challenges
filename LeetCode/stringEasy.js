308688e935cd70aca877c062b8ca48ca61c6dc6e
DenilsonDesigns <daniellanguilller@gmail.com>
2022-06-03 15:18:47 +1000

    1 problem

--- LeetCode/stringEasy.js
+++ LeetCode/stringEasy.js
@@ -1378,8 +1378,28 @@ function canBeTypedWords(text, brokenLetters) {
   return r;
 }
 
+// 1876. Substrings of Size Three with Distinct Characters
+// A string is good if there are no repeated characters.
+
+// Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+
+// Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+
+// A substring is a contiguous sequence of characters in a string.
+function countGoodSubstrings(s) {
+  let r = 0;
+
+  for (let i = 0; i < s.length - 2; i++) {
+    if (s[i] !== s[i + 1] && s[i + 1] !== s[i + 2] && s[i] !== s[i + 2]) {
+      r++;
+    }
+  }
+
+  return r;
+}
+
 console.log(
   // **********************
-  canBeTypedWords("hello world", "ad")
+  countGoodSubstrings("aababcabc")
   // **********************
 );
