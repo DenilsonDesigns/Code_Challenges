448750ae40dc7f2d70f4ac051a862b18ea97a887
DenilsonDesigns <daniellanguilller@gmail.com>
2023-01-10 21:53:51 +1100

    merge two linked lists

--- LeetCode/linkedLists.js
+++ LeetCode/linkedLists.js
@@ -1,7 +1,7 @@
 // *** SETUP CODE:
-class Node {
-  constructor(value, next) {
-    this.value = value;
+class ListNode {
+  constructor(val, next) {
+    this.val = val;
     this.next = next ? next : null;
   }
 }
@@ -11,12 +11,12 @@ const initializeLinkListFromArray = (array) => {
     return [];
   }
 
-  let head = new Node(array[0]);
+  let head = new ListNode(array[0]);
 
   let current = head;
 
-  for (let i = 0; i < array.length; i++) {
-    let newNode = new Node(array[i]);
+  for (let i = 1; i < array.length; i++) {
+    let newNode = new ListNode(array[i]);
     current.next = newNode;
     current = current.next;
   }
@@ -28,7 +28,7 @@ const printLinkedList = (head) => {
   let printStr = "";
 
   while (current) {
-    printStr += current.value + "->";
+    printStr += current.val + "->";
     current = current.next;
   }
 
@@ -78,7 +78,35 @@ function reverseList(head) {
   return previous;
 }
 
+// 21. Merge Two Sorted Lists
+// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists in a one sorted list. The list should be made by
+// splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+function mergeTwoLists(list1, list2) {
+  // make new node which will become the returned list:
+  let list = new ListNode();
+  let head = list;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      list.next = list1;
+      list1 = list1.next;
+    } else {
+      list.next = list2;
+      list2 = list2.next;
+    }
+    list = list.next;
+  }
+
+  list.next = list1 || list2;
+  return head.next;
+}
+
 // setup:
-const head = initializeLinkListFromArray([1, 2, 3, 4, 5]);
+const head1 = initializeLinkListFromArray([1, 5, 6]);
+const head2 = initializeLinkListFromArray([2, 3, 4, 7]);
 // to print:
-printLinkedList(reverseList(head));
+printLinkedList(mergeTwoLists(head1, head2));
