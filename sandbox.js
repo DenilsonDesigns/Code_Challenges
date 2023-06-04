a9be3f5304d47ae38edf297efa902509b4ccee94
Daniel Languiller <daniel.languiller@myob.com>
2023-06-04 14:01:06 +1000

    algoproducts challenge

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,16 @@
-console.log("test");
+function firstFunc(str) {
+  return str.split(" ");
+}
+
+function secondFunc(str) {
+  const reversedStr = reverseString(str);
+
+  return reversedStr;
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(firstFunc("asdf"));
+console.log(secondFunc("asdf"));
