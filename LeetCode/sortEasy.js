a39d2883d5d27eda3d24d1f5068c5efe706027d3
Daniel Languiller <daniel.languiller@myob.com>
2023-12-17 11:32:59 +1100

    LeetCode 2724

--- LeetCode/sortEasy.js
+++ LeetCode/sortEasy.js
@@ -1,16 +1,4 @@
 // 1528. Shuffle String
-
-// Given a string s and an integer array indices of the same length.
-
-// The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
-
-// Return the shuffled string.
-/**
- *
- * @param {string} s
- * @param {array[integers]} indices
- * @returns {string[]}
- */
 function restoreString(s, indices) {
   // Time: O(n) | Space: O(n)
   let r = new Array(s.length);
@@ -22,4 +10,13 @@ function restoreString(s, indices) {
   return r.join("");
 }
 
-console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
+// 2724. Sort By
+var sortBy = function (arr, fn) {
+  return arr.sort((a, b) => fn(a) - fn(b));
+};
+
+console.log(
+  // ***
+  sortBy([5, 4, 1, 2, 3], (x) => x)
+  // ***
+);
