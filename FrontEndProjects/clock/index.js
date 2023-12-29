83e8fed333c9eff6465eb35b39bd9851bca28dfa
Daniel Languiller <daniel.languiller@myob.com>
2023-12-29 17:04:32 +1100

    finish clock kata

--- FrontEndProjects/clock/index.js
+++ FrontEndProjects/clock/index.js
@@ -1,15 +1,19 @@
 const SECONDS_IN_MINUTE = 60;
 const MINUTES_IN_HOUR = 60;
 const HOURS_IN_CLOCK_DAY = 12;
-const DEGREES_PER_SECOND = 360 / SECONDS_IN_MINUTE; // how much second hand rotates per second.
-const DEGREES_PER_MINUTE = 360 / MINUTES_IN_HOUR; // how much minute hand rotates per minute.
-const DEGREES_PER_HOUR = 360 / HOURS_IN_CLOCK_DAY; // how much hour hand rotaets per minute.
+const DEGREES_PER_SECOND = 360 / SECONDS_IN_MINUTE;
+const DEGREES_PER_MINUTE = 360 / MINUTES_IN_HOUR;
+const DEGREES_PER_HOUR = 360 / HOURS_IN_CLOCK_DAY;
 
 const secondHand = document.getElementsByClassName("second-hand")[0];
 const minuteHand = document.getElementsByClassName("minute-hand")[0];
 const hourHand = document.getElementsByClassName("hour-hand")[0];
 
-setInterval(updateClock, 1000);
+const clockBody = document.getElementsByClassName("clock-body")[0];
+
+generateFaceNumbers(clockBody, 4);
+
+setInterval(updateClock, 1);
 
 function updateClock() {
   const currentTime = new Date();
@@ -27,4 +31,21 @@ function updateClock() {
 
 function updateHand(elementToUpdate, param) {
   elementToUpdate.style.transform = `rotate(${param}deg)`;
+}
+
+function generateFaceNumbers(container, numsOnFace = 12) {
+  const increment = 12 / numsOnFace;
+
+  for (var i = 12; i >= 1; i -= increment) {
+    var paragraph = document.createElement("p");
+    paragraph.style.setProperty("--n", i);
+
+    var span = document.createElement("span");
+    span.style.setProperty("--inner", i);
+    span.textContent = i;
+
+    paragraph.appendChild(span);
+
+    container.appendChild(paragraph);
+  }
 }
