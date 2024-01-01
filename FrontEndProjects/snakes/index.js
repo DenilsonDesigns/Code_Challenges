0f3f7b1e016d49d5ce3adfea15d2fbaafa260854
Daniel Languiller <daniel.languiller@myob.com>
2024-01-01 11:51:52 +1100

    snake wip

--- FrontEndProjects/snakes/index.js
+++ FrontEndProjects/snakes/index.js
@@ -5,34 +5,66 @@ import Snake from "./Snake.js";
 const CANVAS_SIZE = 12;
 const GAME_SPEED = 1000;
 const SNEK_BODY_CHAR = "■";
-// const NUM_FRUITS_ON_SCREEN = 1;
+const NUM_FRUITS_ON_SCREEN = 2;
+const FRUIT_CHAR = "ο";
 
 // *** DOM ELEMENTS
 const canvas = document.getElementById("canvas");
+let cells;
 
 // *** GAME ELEMENTS
-// generate snek body based on CANVAS_SIZE;
 const snekQueue = generateStartingSnekQueue(CANVAS_SIZE);
 const snek = new Snake(snekQueue);
-console.log(snek);
+const fruitCoords = generateStartingFruits(
+  NUM_FRUITS_ON_SCREEN,
+  snek.body.items,
+  CANVAS_SIZE
+);
 
 /// *** MAIN LOOP ***
 main();
 
+// *** EVENT LISTENERS
+document.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "ArrowLeft":
+      if (snek.direction === "R") {
+        break;
+      }
+      snek.direction = "L";
+      break;
+    case "ArrowUp":
+      if (snek.direction === "D") {
+        break;
+      }
+      snek.direction = "U";
+      break;
+    case "ArrowRight":
+      if (snek.direction === "L") {
+        break;
+      }
+      snek.direction = "R";
+      break;
+    case "ArrowDown":
+      if (snek.direction === "U") {
+        break;
+      }
+      snek.direction = "D";
+      break;
+  }
+});
+
 function main() {
-  paintCanvas(CANVAS_SIZE);
-  renderSnek();
-  // maybe just generate the coords, then place them in an array globally.
-  // then update snek will check for them,
-  //   placeFruits(NUM_FRUITS_ON_SCREEN, snek);
+  createCanvas(CANVAS_SIZE);
+  renderCanvasElements();
 
   setInterval(() => {
     updateSnek();
-    renderSnek();
+    renderCanvasElements();
   }, GAME_SPEED);
 }
 
-function paintCanvas(gridSize = 10) {
+function createCanvas(gridSize = 10) {
   for (let y = 0; y < gridSize; y++) {
     const row = document.createElement("div");
     row.classList.add("row");
@@ -65,11 +97,48 @@ function paintCanvas(gridSize = 10) {
     }
     canvas.appendChild(row);
   }
+  cells = document.querySelectorAll(".cell");
 }
 
-function renderSnek() {
-  const cells = document.querySelectorAll(".cell");
+function generateStartingFruits(numFruits, snakeBodyCoords, canvasSize) {
+  const r = [];
 
+  while (r.length < numFruits) {
+    const currentCoords = generateRandomXY(canvasSize);
+    console.log(currentCoords);
+    if (!checkCoordExists(currentCoords, snakeBodyCoords)) {
+      r.push(currentCoords);
+    }
+    continue;
+  }
+  return r;
+}
+
+function checkCoordExists(targetCoord, coordsToCheck) {
+  return coordsToCheck.some((array) =>
+    array.every((value, index) => value === targetCoord[index])
+  );
+}
+
+function removeArrayIfExists(targetCoord, coordsToCheck) {
+  const indexToRemove = coordsToCheck.findIndex(
+    (array) =>
+      array.length === targetCoord.length &&
+      array.every((value, index) => value === targetCoord[index])
+  );
+
+  if (indexToRemove !== -1) {
+    coordsToCheck.splice(indexToRemove, 1);
+  }
+}
+
+function generateRandomXY(canvasSize) {
+  let xCoord = Math.floor(Math.random() * canvasSize);
+  let yCoord = Math.floor(Math.random() * canvasSize);
+  return [xCoord, yCoord];
+}
+
+function renderCanvasElements() {
   cells.forEach((cell) => {
     const cellCoordinates = cell.id.split("-").map((coord) => parseInt(coord));
 
@@ -78,61 +147,66 @@ function renderSnek() {
         coord[0] === cellCoordinates[0] && coord[1] === cellCoordinates[1]
     );
 
+    const isFruit = fruitCoords.some(
+      (coord) =>
+        coord[0] === cellCoordinates[0] && coord[1] === cellCoordinates[1]
+    );
+
     if (isSnekBodyPart) {
       cell.innerHTML = SNEK_BODY_CHAR;
+    } else if (isFruit) {
+      cell.innerHTML = FRUIT_CHAR;
     } else {
       cell.innerHTML = "";
     }
   });
 }
 
-// function placeFruits(numFruits, currentSnek){
-//     // generate random spot on screen which is diff from any coords of starting snek,
-// }
-
 function updateSnek() {
-  console.log("updatingsnek size");
   const currentDirection = snek.direction;
   const currentHead = snek.body.getHead();
   const newHead = [...currentHead];
-  console.log("newHead: ", newHead);
+
   console.log("snek: ", snek);
 
   if (currentDirection === "D") {
     if (currentHead[1] === CANVAS_SIZE - 1) {
       snek.body.enQueue([newHead[0], 0]);
-      return snek.body.deQueue();
     }
     snek.body.enQueue([newHead[0], newHead[1] + 1]);
-    return snek.body.deQueue();
   }
 
   if (currentDirection === "U") {
     if (currentHead[1] === 0) {
       snek.body.enQueue([newHead[0], CANVAS_SIZE - 1]);
-      return snek.body.deQueue();
     }
     snek.body.enQueue([newHead[0], newHead[1] - 1]);
-    return snek.body.deQueue();
   }
 
   if (currentDirection === "L") {
     if (currentHead[0] === 0) {
       snek.body.enQueue([CANVAS_SIZE - 1, newHead[1]]);
-      return snek.body.deQueue();
     }
     snek.body.enQueue([newHead[0] - 1, newHead[1]]);
-    return snek.body.deQueue();
   }
 
   if (currentDirection === "R") {
     if (currentHead[0] === CANVAS_SIZE - 1) {
       snek.body.enQueue([0, newHead[1]]);
-      return snek.body.deQueue();
     }
     snek.body.enQueue([newHead[0] + 1, newHead[1]]);
-    return snek.body.deQueue();
   }
+
+  // if newHead is equal to fruit coords, pop from fruit coords.
+  // and DO NOT DEQUEUE;
+  if (checkCoordExists(newHead, fruitCoords)) {
+    // pop newHead from fruitCoords.
+    removeArrayIfExists(newHead, fruitCoords);
+    console.log("fruit Coords: ", fruitCoords);
+    return;
+  }
+
+  return snek.body.deQueue();
 }
 
 function generateStartingSnekQueue(canvasSize) {
