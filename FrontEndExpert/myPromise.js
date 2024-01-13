d073e058aff63a1aa738b60d7aa5a45da6fbaabd
Daniel Languiller <daniel.languiller@myob.com>
2024-01-13 13:43:30 +1100

    notifications wip

--- FrontEndExpert/myPromise.js
+++ FrontEndExpert/myPromise.js
@@ -5,89 +5,47 @@ const STATE = {
 };
 
 class MyPromise {
-  // Write your code here.
-  #state = STATE.PENDING;
-  #value = null;
-  #thenCallback = null;
-  #catchCallback = null;
-
   constructor(executorFunc) {
-    try {
-      executorFunc(
-        (value) => this.#resolve(value),
-        (value) => this.#reject(value)
-      );
-    } catch (error) {
-      this.#reject(error);
-    }
+    // Write your code here.
   }
+
   then(onFulfilled, onRejected) {
-    return new MyPromise((resolve, reject) => {
-      const onFulfilledWithResolve = () => {
-        if (!onFulfilled) return resolve(this.#value);
-
-        queueMicrotask(() => {
-          try {
-            const value = onFulfilled(this.#value);
-            resolve(value);
-          } catch (error) {
-            reject(error);
-          }
-        });
-      };
-
-      const onRejectedWithResolve = () => {
-        if (!onRejected) return reject(this.#value);
-
-        queueMicrotask(() => {
-          try {
-            const value = onRejected(this.#value);
-            resolve(value);
-          } catch (error) {
-            reject(error);
-          }
-        });
-      };
-
-      if (this.#state === STATE.PENDING) {
-        this.#thenCallback = onFulfilledWithResolve;
-        this.#catchCallback = onRejectedWithResolve;
-      } else if (this.#state === STATE.FULFILLED) {
-        onFulfilledWithResolve();
-      } else if (this.#state === STATE.REJECTED) {
-        onRejectedWithResolve();
-      }
-    });
+    // Write your code here.
   }
 
   catch(onRejected) {
-    return this.then(null, onRejected);
+    // Write your code here.
   }
 
   get state() {
-    return this.#state;
+    // Write your code here.
   }
 
   get value() {
-    return this.#value;
-  }
-
-  #resolve(value) {
-    this.#value = value;
-    this.#state = STATE.FULFILLED;
-    if (this.#thenCallback) {
-      this.#thenCallback();
-    }
-  }
-
-  #reject(value) {
-    this.#value = value;
-    this.#state = STATE.REJECTED;
-    if (this.#catchCallback) {
-      this.#catchCallback();
-    }
+    // Write your code here.
   }
 }
+
+const promise = new MyPromise((res, rej) => {
+  res(10);
+});
+
+promise
+  .then((val) => {
+    console.log(val);
+    return val + 10;
+  })
+  .then((val) => {
+    console.log(val);
+    throw val + 10;
+  })
+  .catch((val) => {
+    console.log(val);
+    return val + 10;
+  })
+  .then((val) => {
+    console.log(val);
+  });
 
 // Do not edit the line below.
 exports.MyPromise = MyPromise;
