3b260859fd5e39463fe7994e6976967ac6f74d17
Daniel Languiller <daniel.languiller@myob.com>
2024-01-27 19:57:41 +1100

    finished dot clicker

--- FrontEndProjects/dot_hunter/index.js
+++ FrontEndProjects/dot_hunter/index.js
@@ -1,18 +1,19 @@
-// @TODO: make the dot have concentric circles.
-// @TODO: display: none for dot
-
 //  *** Game Constants ***
+let timeElapsed = 0;
+const MAX_TIME = 10;
 
 // *** DOM Elements ***
-const dot = document.getElementById("dot-outer");
+const body = document.getElementsByTagName("body")[0];
 const canvas = document.getElementById("canvas");
+const progressBar = document.getElementById("bar-progress");
+const dot = document.getElementById("dot-outer");
 
 // *** Event Listeners ***
-dot.addEventListener("click", () => {
-  console.log("dot clicked");
-  dotClickedHandler();
-});
+dot.addEventListener("click", dotClickedHandler);
 
+canvas.addEventListener("click", () => {
+  canvasClickedHandler();
+});
 // *** Main Game Loop ***
 gameLoop();
 
@@ -20,13 +21,27 @@ gameLoop();
 function gameLoop() {
   const [xCoord, yCoord] = getCoordinatesInsideCanvas();
   renderDot({ xCoord, yCoord });
+  renderTimerBar();
 }
 
 function renderDot({ xCoord, yCoord }) {
   dot.style.display = "flex";
   dot.style.position = "absolute";
-  dot.style.top = `${yCoord + 31}px`;
+  dot.style.top = `${yCoord}px`;
   dot.style.left = `${xCoord}px`;
+}
+
+function renderTimerBar() {
+  timeElapsed += 6;
+  requestAnimationFrame(renderTimerBar);
+
+  const unitsElapsed = timeElapsed / 1000;
+
+  const barWidth = Math.max(MAX_TIME - unitsElapsed, 0) * 10;
+  if (barWidth === 0) {
+    gameOverHandler();
+  }
+  progressBar.style.width = `${barWidth}%`;
 }
 
 function getCoordinatesInsideCanvas() {
@@ -35,12 +50,25 @@ function getCoordinatesInsideCanvas() {
 
   const xCoord = Math.random() * (canvasWidth - 30);
   const yCoord = Math.random() * (canvasHeight - 30);
-  console.log(xCoord, yCoord);
   return [xCoord, yCoord];
 }
 
-function dotClickedHandler() {
-  // get new coordinates
+function dotClickedHandler(e) {
+  e.stopPropagation();
   const [xCoord, yCoord] = getCoordinatesInsideCanvas();
   renderDot({ xCoord, yCoord });
+  timeElapsed -= 1000;
+}
+
+function canvasClickedHandler() {
+  body.style.backgroundColor = "red";
+  setTimeout(() => {
+    body.style.backgroundColor = "rgba(56, 125, 125, 0.418)";
+  }, 100);
+  timeElapsed += 1000;
+}
+
+function gameOverHandler() {
+  body.style.backgroundColor = "red";
+  dot.removeEventListener("click", dotClickedHandler);
 }
