703ebcec15a0c231d6ed150b5f063310afb79b6f
Daniel Languiller <daniel.languiller@myob.com>
2024-05-18 15:06:01 +1000

    debounce

--- LeetCode/javascriptMed.js
+++ LeetCode/javascriptMed.js
@@ -1,11 +1,23 @@
-// 2722. Join Two Arrays by ID
-var join = function (arr1, arr2) {};
+// 2627. Debounce
+var debounce = function (fn, t) {
+  let timeOutId;
+
+  return function (...args) {
+    clearTimeout(timeOutId);
+    timeOutId = setTimeout(() => {
+      fn(...args);
+    }, t);
+  };
+};
 
 console.log(
   // ***
-  join(
-    [{ id: 1, b: { b: 94 }, v: [4, 3], y: 48 }],
-    [{ id: 1, b: { c: 84 }, v: [1, 3] }]
+  debounce(
+    [
+      { t: 50, inputs: [1] },
+      { t: 75, inputs: [2] },
+    ],
+    50
   )
   // ***
 );
