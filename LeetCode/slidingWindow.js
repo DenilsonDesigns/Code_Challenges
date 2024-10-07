3ebd062d69a515f0114a9642d89f8d591f4d96c0
Daniel Languiller <daniel.languiller@myob.com>
2024-10-07 21:18:34 +1100

    another sliding window

--- LeetCode/slidingWindow.js
+++ LeetCode/slidingWindow.js
@@ -56,8 +56,71 @@ var divisorSubstrings = function (num, k) {
   }
 };
 
+// 3090 Maximum Length Substring with two occurrences
+var maximumLengthSubstring = function (s) {
+  let r = 1;
+
+  let left = 0;
+  let right = 0;
+
+  let stack = [s[left]];
+
+  let charMap = new Map();
+  charMap.set(s[left], 1);
+
+  while (right < s.length) {
+    if (checkMaxTwoOccMax(charMap, stack)) {
+      r = Math.max(r, stack.length);
+      right++;
+      stack.push(s[right]);
+      incrementKey(charMap, s[right]);
+    } else {
+      left++;
+      right++;
+
+      let valueToDecrement = stack.shift();
+      decrementKey(charMap, valueToDecrement);
+      stack.push(s[right]);
+      incrementKey(charMap, s[right]);
+    }
+  }
+
+  return r;
+
+  function checkMaxTwoOccMax(map, strStack) {
+    for (let i = 0; i < strStack.length; i++) {
+      const char = strStack[i];
+
+      const charOccs = map.get(char);
+
+      if (charOccs > 2) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function incrementKey(map, key) {
+    if (map.has(key)) {
+      map.set(key, map.get(key) + 1);
+    } else {
+      map.set(key, 1);
+    }
+  }
+
+  function decrementKey(map, key) {
+    if (map.has(key)) {
+      const currentValue = map.get(key);
+
+      map.set(key, Math.max(0, currentValue - 1));
+    } else {
+      map.set(key, 0);
+    }
+  }
+};
+
 console.log(
   // ***
-  divisorSubstrings(430043, 2) // 2
+  maximumLengthSubstring("bcbbbcba") // 4
   // ***
 );
