617d8ae2fb0de7241f75173e952a903c03480891
DenilsonDesigns <daniellanguilller@gmail.com>
2026-01-03 08:36:14 +1100

    hoisting and scope questions

---
+++ Hoisting/index.js
@@ -0,0 +1,39 @@
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var a = 1;
+// let b = 2;
+// const c = 3;
+
+// ***
+// let a;
+// console.log(a);
+// console.log(bar());
+
+// a = foo();
+
+// function foo() {
+//   return "foo";
+// }
+
+// var bar = function () {
+//   return "bar";
+// };
+
+// ***
+console.log(a);
+console.log(b);
+
+var a = 1;
+let b = 2;
+
+function test() {
+  console.log(a);
+  console.log(b);
+
+  var a = 3;
+  let b = 4;
+}
+
+test();
---
+++ Scope/index.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log("var:", i);
+  }, 0);
+}
+
+for (let j = 0; j < 3; j++) {
+  setTimeout(() => {
+    console.log("let:", j);
+  }, 0);
+}
